e1d3ddf61dd36f095d8ed00032a3dcd88c157671
Kirill Shishkin <kirillshishkin77@gmail.com>
2021-07-29 20:01:33 +0300

    fixed swagger ui inheritance

--- src/main/java/com/mathinjection/authservice/dto/BaseResponseDto.java
+++ src/main/java/com/mathinjection/authservice/dto/BaseResponseDto.java
@@ -1,10 +1,12 @@
 package com.mathinjection.authservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 import java.time.LocalDateTime;
 
+@Schema(type = "object", name = "Response super class", subTypes = {RegisterResponseDto.class, ErrorResponseDto.class})
 @Data
 @Accessors(chain = true)
 public abstract class BaseResponseDto {
