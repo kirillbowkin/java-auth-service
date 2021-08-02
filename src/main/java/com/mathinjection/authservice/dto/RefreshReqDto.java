6ee46ad043bc9d0ce107f0b892b00d9915b10309
Kirill Shishkin <kirillshishkin77@gmail.com>
2021-08-03 01:17:17 +0300

    adjusted OpenAPI documentation (Auth & Response Bodies)(2)

---
+++ src/main/java/com/mathinjection/authservice/dto/RefreshReqDto.java
@@ -0,0 +1,10 @@
+package com.mathinjection.authservice.dto;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class RefreshReqDto {
+    private String refreshToken;
+}
